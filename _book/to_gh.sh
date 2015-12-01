fe2bfe8df44c2c85e4d9b097abedd1053b651884
hehaiyangwork <hehaiyangwork@qq.com>
2015-12-01 11:39:07 +0800

    _book

--- _book/to_gh.sh
+++ _book/to_gh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 gitbook build .
-cp -rf _book/* ~/tmp/gitbook
-cd ~/tmp/gitbook
+cp -rf _book/* ~/GitHub/MyGitBookBuild
+cd ~/GitHub/MyGitBookBuild
 git add .
 git ci -m 'pub'
 git push origin gh-pages
