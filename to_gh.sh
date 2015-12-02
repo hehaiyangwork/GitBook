159259765b250f976576f68bc134416e4f3b5c20
hehaiyang <hehaiyang@terminus.io>
2015-12-02 19:37:01 +0800

    pub

--- to_gh.sh
+++ to_gh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 gitbook build .
-cp -rf _book/* ~/GitHub/MyGitBookBuild
-cd ~/GitHub/MyGitBookBuild
+cp -rf _book/* ~/GitHub/MyGitBookPages
+cd ~/GitHub/MyGitBookPages
 git add .
 git commit -m 'pub'
 git push origin gh-pages
